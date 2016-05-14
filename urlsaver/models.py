6f5a42ddbb44da3e63eb3c93e5d92e32f4a90b12
samitnuk <samitnuk.ua@gmail.com>
2016-05-14 16:50:00 +0300

    add __str__ to Locator model

--- urlsaver/models.py
+++ urlsaver/models.py
@@ -10,3 +10,6 @@ class Locator(models.Model):
     groupname = models.CharField(max_length=25)
     date = models.DateTimeField(auto_now_add=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
