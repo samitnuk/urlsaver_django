b65a6554d53d5513c33589930f0e959b8ce101be
samitnuk <samitnuk.ua@gmail.com>
2016-05-14 19:03:15 +0300

    clean up code

--- urlsaver/forms.py
+++ urlsaver/forms.py
@@ -1,13 +1,24 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
-#----------------------------------------------------------------------------
+
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-#----------------------------------------------------------------------------
+    clean(self):
+        email = self.cleaned_data['email']
+        user = User.objects.get(email=email)
+        if user is None:
+            raise forms.ValidationError('Invalid user.')
+
+        password = self.cleaned_data['password']
+        if not user.check_password():
+            raise forms.ValidationError('Invalid password.')
+
+
+
 class RegistrationForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -17,16 +28,24 @@ class RegistrationForm(forms.Form):
         email = self.cleaned_data['email']
         user = User.objects.get(email=email)
         if user is not None:
-            raise forms.ValidationError("This email already registered.")
+            raise forms.ValidationError('This email already registered.'"')
 
     clean_confirm(self):
         password = self.cleaned_data['password']
         confirm = self.cleaned_data['confirm']
         if password != confirm:
-            raise forms.ValidationError("'Passwords must match.'")
+            raise forms.ValidationError('Passwords must match.')
 
-#----------------------------------------------------------------------------
+
 class EditForm(forms.Form):
     title = forms.CharField(max_length=500)
     url = forms.CharField(max_length=500)
     groupname = forms.CharField(max_length=25, required=False)
+
+
+class SearchForm(forms.Form):
+    pass
+
+
+class RestorePasswordForm(forms.Form):
+    pass
--- urlsaver/views.py
+++ urlsaver/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from django.http import HttpResponse
+
 
 def main_view(request):
     return HttpResponse("Hello, World! This is main page!")
 
+
 def register_view(reguest):
     return HttpResponse("Register View")
+
 
 def login_view(reguest):
     return HttpResponse("Login View")
 
+
+@login_required
 def logout_view(reguest):
     return HttpResponse("Logout View")
