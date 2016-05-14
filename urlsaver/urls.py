07bda8adbeb798dfd100b63a784e14a00cf33927
samitnuk <samitnuk.ua@gmail.com>
2016-05-14 17:22:53 +0300

    add new views to urls

--- urlsaver/urls.py
+++ urlsaver/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_view, name='main'),
+    url(r'^register/', views.register_view, name='register'),
+    url(r'^login/', views.login_view, name='login'),
+    url(r'^logout/', views.logout_view, name='logout'),
 ]
