33d75989fc70e1c4f46a6a3fb7d675b5b5b178a9
samitnuk <samitnuk.ua@gmail.com>
2016-05-14 17:22:13 +0300

    add base for register/login/logout views

--- urlsaver/views.py
+++ urlsaver/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def main_view(request):
     return HttpResponse("Hello, World! This is main page!")
+
+def register_view(reguest):
+    return HttpResponse("Register View")
+
+def login_view(reguest):
+    return HttpResponse("Login View")
+
+def logout_view(reguest):
+    return HttpResponse("Logout View")
