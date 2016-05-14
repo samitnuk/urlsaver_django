0dde993b53404142c747e8fc27e182e985468117
samitnuk <samitnuk.ua@gmail.com>
2016-05-14 20:43:26 +0300

    create register_view

--- urlsaver/views.py
+++ urlsaver/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.models import User
 
 from django.http import HttpResponse
 
+from .forms import (LoginForm, RegistrationForm,
+                    EditForm, SearchForm, RestorePasswordForm)
 
 def main_view(request):
     return HttpResponse("Hello, World! This is main page!")
 
 
-def register_view(reguest):
-    return HttpResponse("Register View")
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            # as username will be used part of email before '@'
+            username = email.split('@')[0]
+            password = form.cleaned_data['password']
+            user = User(username=username, password=password, email=email)
+            user.save()
+            return redirect('main')
+    else: # if a GET (or any other method) we'll create a blank form
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
 
 
 def login_view(reguest):
