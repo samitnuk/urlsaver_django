628338ef86d30078c5262c21807fcfade318ee5e
Basil Dubyk <samitnuk.ua@gmail.com>
2016-05-14 10:17:05 +0300

    add Locator model

--- urlsaver/models.py
+++ urlsaver/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# class with name URL does not work with Flask in previous project
+# (URL - Uniform Resource Locator)
+class Locator(models.Model):
+    url = models.CharField(max_length=500)
+    title = models.CharField(max_length=500)
+    groupname = models.CharField(max_length=25)
+    date = models.DateTimeField(auto_now_add=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
