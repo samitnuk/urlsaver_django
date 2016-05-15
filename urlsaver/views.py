2cf7ea0ab1f4fcaf259a101175c9a3198e755ffd
Basil Dubyk <samitnuk.ua@gmail.com>
2016-05-15 15:41:21 +0300

    add changes to login_view

--- urlsaver/views.py
+++ urlsaver/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
@@ -20,9 +21,10 @@ def register_view(request):
             # as username will be used part of email before '@'
             username = email.split('@')[0]
             password = form.cleaned_data['password']
-            user = User(username=username, password=password, email=email)
-            user.save()
-            return redirect('main') # name for main_view in urls
+            user = User.objects.create_user(username, email, password)
+            # user = User(username=username, password=password, email=email)
+            # user.save()
+            return redirect('login') # name for login_view in urls
     else: # if a GET (or any other method) we'll create a blank form
         form = RegistrationForm()
 
@@ -32,11 +34,21 @@ def register_view(request):
 def login_view(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
-    else:
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            # as username will be used part of email before '@'
+            username = email.split('@')[0]
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('main') # name for main_view in urls
+    else: # if a GET (or any other method) we'll create a blank form
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
 
-@login_required
+@login_required(login_url='/login/')
 def logout_view(request):
     return HttpResponse("Logout View")
