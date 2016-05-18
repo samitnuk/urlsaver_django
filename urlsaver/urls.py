6f7f87129599466d067cb81388f5c6a3987577d2
samitnuk <samitnuk.ua@gmail.com>
2016-05-18 20:23:37 +0300

    add contact/ and about/

--- urlsaver/urls.py
+++ urlsaver/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_view, name='main'),
-    url(r'^(?P<path>.+)', views.main_view_with_path, name='main_with_path'),
-    url(r'^register/', views.register_view, name='register'),
-    url(r'^login/', views.login_view, name='login'),
-    url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^$', views.main, name='main'),
+    url(r'^(?P<path>.+)', views.main_with_path, name='main_with_path'),
 ]
